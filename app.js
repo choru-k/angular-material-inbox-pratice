3913ac5e83de4004a26cca1719bacbbaaaa78116
g6ling <g6lingp@gmail.com>
2015-12-03 18:32:06 +0900

    2 add comment

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var app = angular.module('StarterApp', ['ngMaterial']);
 
 app.controller('AppController', function($scope) {
-    var vm = this;
 
     //사이드바 열고 닫기를 할수 있게함. 아직은 animation 추가 안함
     $scope.nav = true;
@@ -9,7 +8,7 @@ app.controller('AppController', function($scope) {
         $scope.nav = !$scope.nav;
         console.log($scope.nav);
     };
-
+    //테스트 데이터를 만들었다
     $scope.emails = [
         {from: "kang",
             title: "Title",
@@ -24,6 +23,7 @@ app.controller('AppController', function($scope) {
 
 });
 
+//전체적인 테마색을 red로 바꾸었다 이부분은 자기가 색깔을 조금씩 바꿔보면 바로 알것이다
 app.config(function($mdThemingProvider){
     $mdThemingProvider.theme('default')
         .primaryPalette('red')
