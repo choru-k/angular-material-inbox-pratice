00aba2827727ac293d36955232a4e1ed863bbc59
g6ling <g6lingp@gmail.com>
2015-12-03 17:44:22 +0900

    divide section

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var app = angular.module('StarterApp', ['ngMaterial']);
 
-app.controller('AppController', function($mdSidenav, $scope) {
+app.controller('AppController', function($scope) {
     var vm = this;
 
+    //사이드바 열고 닫기를 할수 있게함. 아직은 animation 추가 안함
     $scope.nav = true;
     $scope.click_toggle = function(){
         $scope.nav = !$scope.nav;
         console.log($scope.nav);
-    };
-    vm.toggleSidenav = function(menuId) {
-        $mdSidenav(menuId).toggle();
     };
 
 });
