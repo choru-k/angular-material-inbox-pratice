6a7d68c1f55b0b356d8bc8b97ffadc950f9dd1e6
g6ling <g6lingp@gmail.com>
2015-12-04 15:47:26 +0900

    5-1 | add fab & add star-mini-window & add a little ofpart of mail-view

--- app.js
+++ app.js
@@ -2,18 +2,57 @@ var app = angular.module('StarterApp', ['ngMaterial']);
 
 app.controller('AppController', function($scope) {
 
-    //사이드바 열고 닫기를 할수 있게함. 아직은 animation 추가 안함
-    $scope.nav = true;
-    $scope.click_toggle = function(){
-        $scope.nav = !$scope.nav;
-        console.log($scope.nav);
-    };
     //테스트 데이터를 만들었다
     $scope.emails = [
         {from: "kang",
-            title: "Title",
+            title: "Title-kang",
             context: "this is context"},
         {from: "lee",
+            title: "Title-lee",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title-1",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title-4",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title",
+            context: "this is context"},
+        {from: "wow",
+            title: "Title",
+            context: "this is context"},
+        {from: "wow",
             title: "Title",
             context: "this is context"},
         {from: "wow",
@@ -21,7 +60,28 @@ app.controller('AppController', function($scope) {
             context: "this is context"}
     ];
 
+    //사이드바 열고 닫기를 할수 있게함. 아직은 animation 추가 안함
+    $scope.nav = true;
+    $scope.click_toggle = function(){
+        $scope.nav = !$scope.nav;
+        console.log($scope.nav);
+    };
 
+    $scope.now_mail_view = false;
+    $scope.up_mail_box = $scope.emails;
+    $scope.down_mail_box = [];
+    $scope.mail_click = function (index) {
+        $scope.up_mail_box = $scope.emails.slice(0,index);
+        $scope.now_mail = $scope.emails[index];
+        $scope.down_mail_box = $scope.emails.slice(index+1,$scope.emails.length);
+        $scope.now_mail_view = true;
+        $scope.now_mail_index = index;
+    };
+
+    $scope.star_show = false;
+    $scope.star_click = function () {
+        $scope.star_show = !$scope.star_show;
+    };
 
 });
 
